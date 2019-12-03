3b2f10375f0f776c17357cf4394d3ee4e43c62a3
RJ <rjsouza1995@gmail.com>
2019-12-02 17:15:57 -0800

    Fix

--- TicTacToeBoardTest.cpp
+++ TicTacToeBoardTest.cpp
@@ -128,14 +128,14 @@ TEST(TicTacToeBoardTest, testCats)
 {
 	TicTacToeBoard board;
 	board.placePiece(0,0);
-	board.placePiece(0,1);
 	board.placePiece(0,2);
-	board.placePiece(1,0);
+	board.placePiece(0,1);
 	board.placePiece(1,1);
-	board.placePiece(1,2);
 	board.placePiece(2,0);
-	board.placePiece(2,1);
+	board.placePiece(1,0);
 	board.placePiece(2,2);
+	board.placePiece(2,1);
+	board.placePiece(1,2);
 
 
 	
